c37d235e23e3b6ccf83a7462a0434a04f5d25fb5
Tahsin <t1m9m@Tahsins-MacBook-Pro.local>
2017-06-15 00:47:54 +0600

    Ajax email working

--- support/send.php
+++ support/send.php
@@ -1,32 +1,19 @@
 <?php
 require '../phpmailer/PHPMailerAutoload.php';
 
-$mail = new PHPMailer;
+$mail 				= 	new PHPMailer;
+$mail->isSMTP();
+$mail->Host 		= 	'smtp.gmail.com';
+$mail->SMTPAuth 	= 	true;
+$mail->Username 	= 	't1m9m.com@gmail.com';
+$mail->Password 	= 	'qyzvmiaxhrdzzwke';
+$mail->SMTPSecure	=	'tls';
+$mail->Port = 587;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 't1m9m.com@gmail.com';                 // SMTP username
-$mail->Password = 'qyzvmiaxhrdzzwke';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
-$mail->setFrom('t1m9m.com@gmail.com', 't1m9m');
-$mail->addAddress('bangladesheee@gmail.com');     // Add a recipient
-// $mail->addAddress('ellen@example.com');               // Name is optional
-// $mail->addReplyTo('info@example.com', 'Information');
-// $mail->addCC('cc@example.com');
-// $mail->addBCC('bcc@example.com');
-
-// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->setFrom($_POST['email']);
+$mail->addAddress('t1m9m.com@gmail.com');
+$mail->isHTML(true);
+$mail->Subject 		= 	$_POST['subject'];
+$mail->Body    		= 	$_POST['description'];
 
 $mail->send();
-?>
